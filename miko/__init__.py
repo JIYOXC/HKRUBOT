3aa94eecaf4d772bf09757c1ca80b37fa086d095
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-09-15 12:08:20 +0700

    Update __init__.py

--- miko/__init__.py
+++ miko/__init__.py
@@ -64,7 +64,7 @@ class Ubot(Client):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs, device_model="ʜɪᴋᴀʀᴜ ᴜʙᴏᴛ")
-        self.group_call = GroupCall(self).get_group_call()
+        self.group_call = GroupCallFactory(self).get_group_call()
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
